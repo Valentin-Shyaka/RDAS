d8179a05b2b3f324450e4a9bde83c4ee47179656
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-17 23:31:30 +0200

    settings page scrapping

--- src/components/pages/Settings.jsx
+++ src/components/pages/Settings.jsx
@@ -29,7 +29,7 @@ const Settings = () => {
     ]
     const Link=({name,text})=>{
         return(
-        <p className={(active&& (text=name)) ?"bg-[#A162F71A] p-2":'p-2'} 
+        <p className={(active&& (text=name)) ?"bg-[#A162F71A] ":''} 
                     onClick={()=>
                         {
                             setActive(!active) 
@@ -39,7 +39,7 @@ const Settings = () => {
     }
     const Field=({Icon,placeholder,label})=>{
         return(
-            <div className='mt-4 w-full'>
+            <div className='mt-2 w-full'>
             <p className='text-slate-300 '>{label}</p>
             <div className='w-full flex border border-slate-400 rounded-lg h-12 items-center gap-4 text-slate-400 p-2 mt-2'>
                 <Icon/>
@@ -49,21 +49,21 @@ const Settings = () => {
         )
     }
   return (
-    <div className='p-8'>
+    <div className='p-2'>
         <NavHeader title={'Settings'} />
-        <div className='flex items-center w-full h-[80vh] gap-20 mt-8'>
+        <div className='flex items-center w-full h-[60%] gap-20 mt-6'>
             <Sidebar/>
             <div className='w-full'>
-                <div className='flex w-full gap-10 text-slate-300 font-bold cursor-pointer h-10'>
+                <div className='flex w-full gap-10 text-slate-300 font-bold cursor-pointer h-4'>
                     {navs.map(nav =>(
                         <Link name={nav.content} text={nav.content}/>
                     ))}
                     
                 </div>
-                <div className='w-[80%] border border-1 border-slate-400 rounded-3xl h-fit p-10 mt-6'>
+                <div className='w-[80%] border border-1 border-slate-400 rounded-3xl h-fit p-4 mt-4'>
                     <h3 className='font-bold'>Profile</h3>
                     <p className='text-sm text-slate-400 mt-2'>Update your photo and personnal details here</p>
-                    <hr className='mt-4 bg-slate-400 '/>
+                    <hr className='mt-2 bg-slate-400 '/>
 
                     <div className='flex w-full gap-40'>
                         <Field label={"Live in"} Icon={HomeOutlinedIcon} placeholder={'Zurich, Switzerland'}/>
@@ -74,20 +74,20 @@ const Settings = () => {
                         <Field label={"Date of Birth"} Icon={CakeOutlinedIcon} placeholder={'25.8.2006'}/>
                         <Field label={"Gender"} Icon={MaleOutlinedIcon} placeholder={'Male'}/>
                     </div>
-                    <hr className='mt-6 bg-slate-400 '/>
-                    <div className='flex mt-4 justify-between  h-20 items-center'>
+                    <hr className='mt-2 bg-slate-400 '/>
+                    <div className='flex mt-2 justify-between  h-16 items-center'>
                         <div>
                             <h3 className='font-bold'>Your Profile</h3>
                             <p className='text-sm text-slate-400'>This will be displayed on your profile.</p>
                         </div>
-                        <img src="./user_profile.png" alt="" srcset="" className='rounded-full w-20 h-20' />
+                        <img src="./user_profile.png" alt="" srcset="" className='rounded-full w-16 h-16' />
                         <div className='flex gap-10 font-bold text-slate-400'>
                             <p>Delete</p>
                             <p className='text-[#A162F7]'>Update</p>
                         </div>
 
                     </div>
-                    <hr className='mt-6 bg-slate-400 '/>
+                    <hr className='mt-4 bg-slate-400 '/>
                     <div className='flex justify-between mt-4'>
                         <h3 className='font-bold'>Social Profiles</h3>
                         <div className=''>
