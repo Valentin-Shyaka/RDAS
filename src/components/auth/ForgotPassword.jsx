2a9bd6151017d1f5db59703aad2a4c01e8fe471f
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-17 23:04:43 +0200

    ForgotPassword page scrapping

--- src/components/auth/ForgotPassword.jsx
+++ src/components/auth/ForgotPassword.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const ForgotPassword = () => {
   return (
     <div className='text-center text-3xl justify-center p-4 flex items-center h-full w-screen gap-10'>
-      <div className='bubble w-[300px] h-[300px] absolute left-[50vw] top-[2vh]'></div>
-      <div className='bubble w-[200px] h-[200px] absolute left-[80vw] top-[75vh]'></div>
+      <div className='bubble w-[200px] h-[200px] absolute left-[42%] top-[2%]'></div>
+      <div className='bubble w-[150px] h-[150px] absolute left-[82%] top-[75%]'></div>
       <div className=' flex flex-col'>
-        <p className=' text-6xl'>No worries.!!</p>
+        <p className=' text-4xl'>No worries.!!</p>
         <div className='flex items-center gap-4'>
-        <button className=' border border-white border-[4px] w-[200px] mt-4 h-16 text-sm'>Take me back.!</button>
-        <div className='w-full border border-4 border-slate-500 border-dashed h-[1px] '></div>
+        <button className=' border border-white border-4 w-80 mt-4 h-16 text-sm'>Take me back.!</button>
+        <div className='w-full border border-4 border-slate-500 border-dashed h-1 '></div>
         </div>
       </div>
-      <div className='ml-20 w-[500px] h-[80vh] auth-glass text-left p-12 z-10'>
+      <div className='ml-20 w-[35%] h-fit auth-glass text-left p-8 z-10'>
         <div>
-        <h3 className='mt-20'>Forgot your Password</h3>
+        <h3 className='mt-2'>Forgot your Password</h3>
         <p className='text-sm mt-4'>Please enter your email.!</p>
         
         <input type='text' placeholder='example@gmail.com' className='mt-4 bg-inherit border border-white rounded-md text-sm h-10 w-full p-2 outline-none'></input>
@@ -22,13 +22,13 @@ const ForgotPassword = () => {
         
         
 
-          <button className='w-full text-sm w-full h-12 mt-8 auth-button '>Reset pasword</button>
+          <button className='w-full text-sm  h-12 mt-8 auth-button '>Reset pasword</button>
           
         </div>
        
-        <div className='text-sm text-center mt-20'>
+        <div className='text-sm text-center mt-6'>
           <p>Dont't have an account? signup</p>
-          <div className='flex gap-6 mt-6'>
+          <div className='flex  mt-6 justify-between'>
             <p>Terms&conditions</p>
             <p>support</p>
             <p>customer care</p>
