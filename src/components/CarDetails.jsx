2c95ad0b890d65a66879ea3c4fd7be5e7ac65acf
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-18 21:52:36 +0200

    CarDetails responsive fix..

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -1,17 +1,17 @@
 const CarDetails = () => {
   return (
-    <div className="flex flex-wrap bg-inherit  items-center justify-center w-full gap-4 lg:gap-6 p-4">
+    <div className="flex flex-wrap bg-inherit  items-center justify-center w-full gap-4 lg:gap-6 p-4 text-sm">
       <div className="flex flex-col border-[3px] rounded-lg items-center  p-4 gap-4">
         <span className="text-3xl font-semibold">Reach Out</span>
         <span className="text-[#CFF80B] text-lg">Your Space</span>
         <div className=" w-[15rem] h-[10rem]">
           <img src="./car.png" alt="h-auto w-full" />
         </div>
-        <div className="flex flex-col w-[23rem] items-center justify-center bg-[#1d6d9b42] p-6 rounded-lg gap-4">
+        <div className="flex flex-col w-[15rem] items-center justify-center bg-[#1d6d9b42] p-6 rounded-lg gap-4">
           <span>Premium and prestige car needs proper care</span>
           <span>Experience the thrill and satisfaction to drive.</span>
         </div>
-        <button className="bg-[#CFF80B] font-semibold w-[23rem] h-[3.3rem] rounded-md">
+        <button className="bg-[#CFF80B] font-semibold w-[15rem] h-[3.3rem] rounded-md">
           LET'S GO
         </button>
       </div>
@@ -19,7 +19,7 @@ const CarDetails = () => {
         <div className="w-[15rem] h-[22rem] bg-[#1d1d1c] rounded-lg">
           <img src="./car.png" alt="" />
         </div>
-        <div className="flex p-4 gap-4">
+        <div className="flex p-4 gap-2">
           <div className="bg-[#1d6d9b15] rounded-md flex flex-col p-4 justify-center gap-4">
             <span>Total Distance</span>
             <span>40,000 Km</span>
@@ -35,21 +35,21 @@ const CarDetails = () => {
           <div className="w-[15rem] h-[60%]">
             <img src="./car.png" alt="" />
           </div>
-          <div className="flex p-4 gap-4">
+          <div className="flex p-4 gap-2">
             <div className="bg-[#1d6d9b42] rounded-md flex flex-col p-4 justify-center gap-4">
               <span>Diesel</span>
               <span>40,000 Km</span>
             </div>
             <div className="bg-[#1d6d9b42] rounded-md flex flex-col p-4 justify-center gap-4">
               <span>Cool Seat</span>
-              <span>Temp Control On Seat</span>
+              <span>Control On Seat</span>
             </div>
           </div>
-          <button className="bg-[#CFF80B] font-semibold w-[23rem] h-[3.3rem] rounded-md">
+          <button className="bg-[#CFF80B] font-semibold w-[15rem] h-[3.3rem] rounded-md">
             REVIEW NOW
           </button>
         </div>
-        <div className="w-[365px] h-[10rem] bg-slate-500 rounded-md">
+        <div className="w-[15rem] h-[10rem] bg-slate-500 rounded-md">
           <img
             src="./gps.jpg"
             alt=""
