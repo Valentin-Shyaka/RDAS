6e041a5db0b2b399e3e1b4117fbf6650ec7cd83b
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-19 00:36:34 +0200

    continous Dashboard implementation...

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -8,8 +8,64 @@ import AirRoundedIcon from '@mui/icons-material/AirRounded';
 import BluetoothRoundedIcon from '@mui/icons-material/BluetoothRounded';
 import SmsRoundedIcon from '@mui/icons-material/SmsRounded';
 import AirlineSeatReclineExtraRoundedIcon from '@mui/icons-material/AirlineSeatReclineExtraRounded';
+import PrivacyTipIcon from '@mui/icons-material/PrivacyTip';
+import Switch, { SwitchProps } from '@mui/material/Switch';
+
+import { styled } from '@mui/material/styles';
 
 const Dashboard = () => {
+    const IOSSwitch =styled((props: SwitchProps) => (
+        <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+      ))(({ theme }) => ({
+        width: 42,
+        height: 26,
+        padding: 0,
+        '& .MuiSwitch-switchBase': {
+          padding: 0,
+          margin: 2,
+          transitionDuration: '300ms',
+          '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+              backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+              opacity: 1,
+              border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+              opacity: 0.5,
+            },
+          },
+          '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+          },
+          '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[600],
+          },
+          '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+          },
+        },
+        '& .MuiSwitch-thumb': {
+          boxSizing: 'border-box',
+          width: 22,
+          height: 22,
+        },
+        '& .MuiSwitch-track': {
+          borderRadius: 26 / 2,
+          backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+          opacity: 1,
+          transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+          }),
+        },
+      }));
+      
+    
   return (
     <div className='p-8'>
         <NavHeader title={'Dashboard'} />
@@ -138,7 +194,19 @@ const Dashboard = () => {
             
             </div>
             <div>
-                <p>dashborad</p>
+                <div className='bg-[#1E1E1E] p-4 flex rounded-xl items-center justify-between gap-4'>
+                    <div className='bg-[#2f2f2f] text-center align-middle p-4 rounded-xl'>
+                        <PrivacyTipIcon className='text-red-500'/>
+                    </div>
+                    <div className='text-sm h-full justify-center'>
+                        <p className='font-bold'>Emergency</p>
+                        <span className='text-slate-400'>Switch ON only in emergency case</span>
+                    </div>
+                    <div>
+                        <IOSSwitch/>
+                    </div>
+
+                </div>
             </div>
             
             
