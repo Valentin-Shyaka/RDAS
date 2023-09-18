54fccd2a81147ce3c039698341834c65807eb3d0
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-18 21:47:51 +0200

    CarDetails responsive fix...

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -4,7 +4,7 @@ const CarDetails = () => {
       <div className="flex flex-col border-[3px] rounded-lg items-center  p-4 gap-4">
         <span className="text-3xl font-semibold">Reach Out</span>
         <span className="text-[#CFF80B] text-lg">Your Space</span>
-        <div className=" w-[20rem] h-[10rem]">
+        <div className=" w-[15rem] h-[10rem]">
           <img src="./car.png" alt="h-auto w-full" />
         </div>
         <div className="flex flex-col w-[23rem] items-center justify-center bg-[#1d6d9b42] p-6 rounded-lg gap-4">
@@ -16,7 +16,7 @@ const CarDetails = () => {
         </button>
       </div>
       <div className="flex flex-col border-[3px] rounded-lg items-center p-4 gap-4">
-        <div className="w-[20rem] h-[22rem] bg-[#1d1d1c] rounded-lg">
+        <div className="w-[15rem] h-[22rem] bg-[#1d1d1c] rounded-lg">
           <img src="./car.png" alt="" />
         </div>
         <div className="flex p-4 gap-4">
@@ -32,7 +32,7 @@ const CarDetails = () => {
       </div>
       <div className="flex flex-col items-center gap-2 p-4 border-[3px] rounded-lg">
         <div className="flex flex-col items-center">
-          <div className="w-[20rem] h-[60%]">
+          <div className="w-[15rem] h-[60%]">
             <img src="./car.png" alt="" />
           </div>
           <div className="flex p-4 gap-4">
