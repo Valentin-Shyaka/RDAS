4ed4058a97f8093702f9717fbdda05d319f07bc5
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-20 01:55:08 +0200

    screen page changes ...

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -219,7 +219,7 @@ const Dashboard = () => {
                             <LockIcon/>
 
                     </div>
-                    <div className='absolute items-center text-center p-4 bg-[#d1f80ba1] top-5 left-[20rem] rounded-full'>
+                    <div className='absolute items-center text-center p-4 bg-[#d1f80bb7] top-5 left-[20rem] rounded-full'>
                             <LockOpenIcon/>
 
                     </div>
--- src/components/pages/Screen.jsx
+++ src/components/pages/Screen.jsx
@@ -17,17 +17,14 @@ const Screen = () => {
             className="w-full h-[60vh] rounded-xl"
           ></iframe>
           <div className=" flex  p-2 w-full bg-[#1E1E1E] mt-2 rounded-xl justify-center gap-6">
-            <div className="flex flex-col h-4/5 w-[15rem] gap-2 p-2 bg-orange-500 cursor-pointer rounded-md justify-center">
+            <div className="flex flex-col  w-[15rem] gap-2 p-2 bg-[#CFF80B] cursor-pointer rounded-md justify-center">
               <p className="text-sm">You are close to the obstacle <span className="text-sm">10 Metres</span></p>
               
             </div>
-            <div className="flex h-4/5 w-[15rem] gap-2 p-2 bg-red-500 cursor-pointer items-center rounded-md justify-center">
+            <div className="flex  w-[15rem] gap-2 p-2 bg-red-500 cursor-pointer items-center rounded-md justify-center">
               <p className="text-2xl font-bold">STOP</p>
             </div>
-            <div className="flex flex-col h-4/5 w-[15rem] gap-2 p-2 px-8 bg-green-500 cursor-pointer rounded-md justify-center">
-              <p className="text-sm">Speed</p>
-              <p className="text-sm">45km/hr</p>
-            </div>
+            
           </div>
         </div>
       </div>
