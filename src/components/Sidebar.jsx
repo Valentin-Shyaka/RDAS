520159f98df43beeae0b3cbb1198575220e9e29f
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-09-19 23:45:12 +0200

    screen navigation link added...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,11 @@ import CameraAltRoundedIcon from '@mui/icons-material/CameraAltRounded';
 import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
 import PlayCircleRoundedIcon from '@mui/icons-material/PlayCircleRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import { useNavigate } from 'react-router-dom';
 
 
 const Sidebar = () => {
+    const navigate=useNavigate()
   return (
     <div className=' md:hidden lg:block'>
         <div className='rounded-full bg-[#505050] w-14 h-14 text-center text-5xl flex-col mt-6 cursor-pointer'>
@@ -18,7 +20,7 @@ const Sidebar = () => {
         <div className='rounded-full bg-[#1E1E1E] w-14 h-14 text-center text-5xl flex-col mt-6 text-[#A1A1A1] cursor-pointer'>
             <LocalPhoneRoundedIcon/>
         </div>
-        <div className='rounded-full bg-[#1E1E1E] w-14 h-14 text-center text-5xl flex-col mt-6 text-[#A1A1A1] cursor-pointer'>
+        <div className='rounded-full bg-[#1E1E1E] w-14 h-14 text-center text-5xl flex-col mt-6 text-[#A1A1A1] cursor-pointer' onClick={()=>navigate('/screen')}>
             <PlayCircleRoundedIcon/>
         </div>
         <div className='rounded-full bg-[#1E1E1E] w-14 h-14 text-center text-5xl flex-col mt-6 text-[#A1A1A1] cursor-pointer'>
